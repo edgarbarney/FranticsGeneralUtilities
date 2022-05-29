0f94d47445f3dcef24c2eefaa4e65bd88d686f8d
FranticDreamer <edwardgmgmd@gmail.com>
2022-05-29 12:33:20 +0300

    Implement Signed Operations and Fix Subtraction Order
    Greatest had to be always _str1, now it doesn't have to.

--- CursedStringMaths/CursedStringMaths.hpp
+++ CursedStringMaths/CursedStringMaths.hpp
@@ -135,17 +135,27 @@ namespace FranUtils
 		inline bool IsPositive(const std::string& _str);
 
 		inline std::string FlipSign(const std::string& _str);
+		inline std::string Absolute(const std::string& _str);
 		inline void FlipSignRef(std::string& _str);
+		inline void AbsoluteRef(std::string& _str);
 
 		// Sometimes str.size() doesn't return the right value
 		// because of the negative sign.
-		inline size_t GetDigits(const std::string& _str);
+		inline size_t GetDigitCount(const std::string& _str);
 
 		// Is A (_str1) greater than B (_str2) ?
 		inline bool GreaterThan(const std::string& _str1, const std::string& _str2);
-
 		// Is A (_str1) greater than or equal to B (_str2) ?
 		inline bool GreaterThanOrEqualTo(const std::string& _str1, const std::string& _str2);
+		// Is A (_str1) less than B (_str2) ?
+		inline bool LessThan(const std::string& _str1, const std::string& _str2);
+		// Is A (_str1) less than or equal to B (_str2) ?
+		inline bool LessThanOrEqualTo(const std::string& _str1, const std::string& _str2);
+
+		inline std::string Max(const std::string& _str1, const std::string& _str2);
+		inline std::string Min(const std::string& _str1, const std::string& _str2);
+
+		// Operations
 
 		inline std::string Subtract(std::string _str1, std::string _str2);
 		inline std::string Add(std::string _str1, std::string _str2);
@@ -172,6 +182,17 @@ namespace FranUtils
 			return retStr;
 		}
 
+		std::string Absolute(const std::string& _str)
+		{
+			std::string retStr = _str;
+
+			if (IsPositive(_str))
+				return retStr;
+			
+			retStr.erase(0, 1);
+			return retStr;
+		}
+
 		void FlipSignRef(std::string& _str)
 		{
 			if (IsPositive(_str))
@@ -180,11 +201,19 @@ namespace FranUtils
 				_str.erase(0, 1);
 		}
 
+		void AbsoluteRef(std::string& _str)
+		{
+			if (IsPositive(_str))
+				return;
+
+			_str.erase(0, 1);
+		}
+
 		// Sometimes str.size() doesn't return the right value
 		// because of the negative sign.
-		size_t GetDigits(const std::string& _str)
+		size_t GetDigitCount(const std::string& _str)
 		{
-			return !IsPositive(_str) ? _str.size() - 1 : _str.size();
+			return IsPositive(_str) ? _str.size() : _str.size() - 1;
 		}
 
 		// Is A (_str1) greater than B (_str2) ?
@@ -194,10 +223,10 @@ namespace FranUtils
 			if (IsPositive(_str1) != IsPositive(_str2))
 				return IsPositive(_str1);
 
-			if (_str1.size() != _str2.size())
-				return GetDigits(_str1) > GetDigits(_str2); // str.size() would work but just in case
+			if (GetDigitCount(_str1) != GetDigitCount(_str2))
+				return GetDigitCount(_str1) > GetDigitCount(_str2); // str.size() would work but just in case
 
-			for (size_t i = !IsPositive(_str1); i < GetDigits(_str1); i++)
+			for (size_t i = !IsPositive(_str1); i < GetDigitCount(_str1); i++)
 			{
 				if (_str1[i] == _str2[i])
 					continue;
@@ -219,10 +248,10 @@ namespace FranUtils
 			if (IsPositive(_str1) != IsPositive(_str2))
 				return IsPositive(_str1);
 
-			if (_str1.size() != _str2.size())
-				return GetDigits(_str1) > GetDigits(_str2); // str.size() would work but just in case
+			if (GetDigitCount(_str1) != GetDigitCount(_str2))
+				return GetDigitCount(_str1) > GetDigitCount(_str2); // str.size() would work but just in case
 
-			for (size_t i = !IsPositive(_str1); i < GetDigits(_str1); i++)
+			for (size_t i = !IsPositive(_str1); i < GetDigitCount(_str1); i++)
 			{
 				if (_str1[i] == _str2[i])
 					continue;
@@ -237,32 +266,67 @@ namespace FranUtils
 			return true;
 		}
 
+		bool LessThan(const std::string& _str1, const std::string& _str2)
+		{
+			return !GreaterThanOrEqualTo(_str1, _str2);
+		}
+
+		bool LessThanOrEqualTo(const std::string& _str1, const std::string& _str2)
+		{
+			return !GreaterThan(_str1, _str2);
+		}
+
+		std::string Max(const std::string& _str1, const std::string& _str2)
+		{
+			if (GreaterThan(_str1, _str2))
+				return _str1;
+
+			return _str2;
+		}
+
+		std::string Min(const std::string& _str1, const std::string& _str2)
+		{
+			if (LessThan(_str1, _str2))
+				return _str1;
+
+			return _str2;
+		}
+
 		std::string Add(std::string _str1, std::string _str2)
 		{
+			bool isPositive = !(GreaterThan(Absolute(_str1), Absolute(_str2)) && !IsPositive(_str1));
+
+			// Do subtraction if signs are different
 			if (IsPositive(_str1) != IsPositive(_str2))
-				if (IsPositive(_str1))
-					return Subtract(_str1, FlipSign(_str2));
-				else
-					return Subtract(FlipSign(_str1), _str2);
+			{
+					std::string retStr = Subtract(Max(Absolute(_str1), Absolute(_str2)), Min(Absolute(_str1), Absolute(_str2)));
+					return isPositive ? retStr : FlipSign(retStr);
+			}
+			
+			// Signs are not different, so we can get just one's sign
+			isPositive = IsPositive(_str1);
+
+			AbsoluteRef(_str1); 
+			AbsoluteRef(_str2);
 
 			std::string finalStr = "";
 
 			uint8_t left = 0;
 			uint8_t tempDigitAdd = 0;
 
-			std::string* bigStrPtr = std::max(GetDigits(_str1), GetDigits(_str2)) == GetDigits(_str1) ? &_str1 : &_str2;
-			std::string* smolStrPtr = std::max(GetDigits(_str1), GetDigits(_str2)) == GetDigits(_str1) ? &_str2 : &_str1;
+			std::string* bigStrPtr = std::max(GetDigitCount(_str1), GetDigitCount(_str2)) == GetDigitCount(_str1) ? &_str1 : &_str2;
+			std::string* smolStrPtr = std::max(GetDigitCount(_str1), GetDigitCount(_str2)) == GetDigitCount(_str1) ? &_str2 : &_str1;
 
 			// Add leading zeroes to the "smaller" string
 			std::ostringstream ss;
-			ss << std::setw(std::max(GetDigits(_str1), GetDigits(_str2))) << std::setfill('0') << *smolStrPtr;
+			ss << std::setw(std::max(GetDigitCount(_str1), GetDigitCount(_str2))) << std::setfill('0') << *smolStrPtr;
 			*smolStrPtr = ss.str();
 
 			// Reverse both strings
 			std::reverse(_str1.begin(), _str1.end());
 			std::reverse(_str2.begin(), _str2.end());
 
-			for (size_t i = 0; i < GetDigits(_str1) - !IsPositive(_str1); i++)
+			for (size_t i = 0; i < GetDigitCount(_str1); i++)
 			{
 				tempDigitAdd = left + (_str1[i] - '0') + (_str2[i] - '0');
 
@@ -284,29 +348,51 @@ namespace FranUtils
 				finalStr.insert(0, 1, left + '0');
 			}
 
-			return finalStr;
+			// Add sign if needed then return
+			return isPositive ? finalStr : finalStr.insert(0, 1, negativeChar);
 		}
 
 		std::string Subtract(std::string _str1, std::string _str2)
 		{
+			bool isPositive = !(GreaterThan(Absolute(_str1), Absolute(_str2)) && !IsPositive(_str1));
+
+			// Do addition if signs are different
+			if (IsPositive(_str1) != IsPositive(_str2))
+			{
+				std::string retStr = Add(Max(Absolute(_str1), Absolute(_str2)), Min(Absolute(_str1), Absolute(_str2)));
+				return isPositive ? retStr : FlipSign(retStr);
+			}
+
+			// Signs are not different, so we can get just one's sign
+			isPositive = IsPositive(_str1);
+
+			if (LessThan(Absolute(_str1), Absolute(_str2)))
+			{
+				std::swap(_str1, _str2);
+				isPositive = !isPositive;
+			}
+
+			AbsoluteRef(_str1);
+			AbsoluteRef(_str2);
+
 			std::string finalStr = "";
 
 			uint8_t need = 0;
 			uint8_t tempDigitSubt = 0;
 
-			std::string* bigStrPtr = std::max(GetDigits(_str1), GetDigits(_str2)) == GetDigits(_str1) ? &_str1 : &_str2;
-			std::string* smolStrPtr = std::max(GetDigits(_str1), GetDigits(_str2)) == GetDigits(_str1) ? &_str2 : &_str1;
+			std::string* bigStrPtr = std::max(GetDigitCount(_str1), GetDigitCount(_str2)) == GetDigitCount(_str1) ? &_str1 : &_str2;
+			std::string* smolStrPtr = std::max(GetDigitCount(_str1), GetDigitCount(_str2)) == GetDigitCount(_str1) ? &_str2 : &_str1;
 
 			// Add leading zeroes to the "smaller" string
 			std::ostringstream ss;
-			ss << std::setw(std::max(GetDigits(_str1), _str2.size())) << std::setfill('0') << *smolStrPtr;
+			ss << std::setw(std::max(GetDigitCount(_str1), _str2.size())) << std::setfill('0') << *smolStrPtr;
 			*smolStrPtr = ss.str();
 
 			// Reverse both strings
 			std::reverse(_str1.begin(), _str1.end());
 			std::reverse(_str2.begin(), _str2.end());
 
-			for (size_t i = 0; i < GetDigits(_str1) - !IsPositive(_str1); i++)
+			for (size_t i = 0; i < GetDigitCount(_str1); i++)
 			{
 				uint8_t ch1 = (_str1[i] - '0');
 				uint8_t ch2 = (_str2[i] - '0') + need;
@@ -329,8 +415,11 @@ namespace FranUtils
 				tempDigitSubt = 0;
 			}
 
-			// Remove leading zeroes then return
-			return finalStr.erase(0, std::min(finalStr.find_first_not_of('0'), finalStr.size() - 1));
+			// Remove leading zeroes
+			finalStr.erase(0, std::min(finalStr.find_first_not_of('0'), finalStr.size() - 1));
+
+			// Add sign if needed then return
+			return isPositive ? finalStr : finalStr.insert(0, 1, negativeChar);
 		}
 	};
 };
